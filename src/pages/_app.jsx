a66e43f255a4f44c1deb5882a0566ed691fadb25
Sun LeeHuor <jkhmer1111@gmail.com>
2023-05-20 10:05:37 +0700

    Added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,9 +5,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-          {/* <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
           <meta http-equiv="X-UA-Compatible" content="IE=7" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
